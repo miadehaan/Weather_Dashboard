1edf9b9b550467fa28864506a3c3436aa716f0e5
miadehaan <64979457+miadehaan@users.noreply.github.com>
2020-08-18 22:48:21 -0700

    both apis work and data is retrieved - top section for current data works

--- script.js
+++ script.js
@@ -7,69 +7,116 @@ $(document).ready(function() {
     // Weather data
     var cityName;
     var country;
-    var tempF;
+    var temp;
     var humidity;
     var windspeed;
     var uvIndex;
-
+    var lon;
+    var lat;
+    var currentIcon;
+    var tempForecast;
+    var humidityForecast;
+    var dailyTemp;
+    var dailyHumidity;
     
+
+    var apiKey = "e68a6c498567148d8870611b117efac1";
 
     // Retrieve the city input by the user
     $(".citySubmit").on("click", function(event){
         event.preventDefault();
+
+        // Get the city the user input
         cityInput = $(".cityInput").val().trim();
 
-
-        // var apiKey = "e68a6c498567148d8870611b117efac1";
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=e68a6c498567148d8870611b117efac1";
-
-        // AJAX call to get the weather data
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid="+ apiKey + "&units=imperial";
+        // First AJAX call to get lat/lon coordinates based on city input
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response) {
-            console.log(response);
+            // console.log(response);
             
-            cityName = response.name;
+            localStorage.setItem("city", response.name);
+            cityName = localStorage.getItem("city");
             country = response.sys.country;
-            // date = response;
-            var tempKelvin = response.main.temp; // in Kelvin
-            tempF = parseFloat(((tempKelvin-273.15)*1.8)+32).toFixed(2);
-            humidity = response.main.humidity;
-            windspeed = response.wind.speed;
-            // uvIndex = response;
+            date = response.dt; //Time of data calculation, unix, UTC
+            lon = response.coord.lon;
+            lat = response.coord.lat;
 
-            current();
-            future();
+            
+            // Second/Main AJAX Call to get weather data
+            var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely&appid=e68a6c498567148d8870611b117efac1";
+            //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=daily&appid=e68a6c498567148d8870611b117efac1&units=imperial
+
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function(response) {
+                console.log(response);
+    
+                // Get current weather data
+                temp = response.current.temp; 
+                humidity = response.current.humidity;
+                windspeed = response.current.speed;
+                uvIndex = response.current.uvi;
+                currentIcon = response.current.weather[0].icon;
+                
+                // Get data for 5-day forecast
+                for (var i=0; i < response.daily.length; i++) {
+                    dailyTemp = response.daily[i].temp.day;
+                    dailyHumidity = response.daily[i].humidity;
+
+
+  
+                }
+
+                
+                current();
+                future(); 
+            });
         }); 
-        
     });
 
-    function current() {
-        // Add city Name to top section (".currentWeather")
 
-        // Add to history and pull info from localStorage
-        localStorage.setItem("city", cityName);
-        var history = $("<div>").text( localStorage.getItem(localStorage.key(0)) );
+    function current() {
+
+        // Pull this data from localStorage ???
+
+        $("#cityName").text(cityName + ", " + country);
+        $(".date").text("(" + date + ")");
+        $("#temp").text("Temperature: " + temp + " F");
+        $("#humidity").text("Humidity: " + humidity + " %");
+        $("#windSpeed").text("Wind Speed: " + windspeed + " mph");
+        $("#uvIndex").text("UV Index: " + uvIndex);
+
+        // Add weather Icon
+        var iconURL = "http://openweathermap.org/img/wn/" + currentIcon + "@2x.png";
+        $(".weatherIcon").attr("src", iconURL);
+
+        
+        
+        // Display the search history and pull info from localStorage
+        var history = $("<div>").text( localStorage.getItem("city") );
         history.attr("border: 1px solid");
         $(".history").append(history);
-        console.log(localStorage.getItem(localStorage.key(0) ));
-
-
-        // Pull this data from API (or localStorage?)
-
-        $("#cityName").html(cityName + ", " + country);
-        $("#date").text(date);
-        $("#temp").text("Temperature: " + tempF + " F");
-        $("#humidity").text("Humidity: " + humidity);
-        $("#windSpeed").text("Wind Speed: " + windspeed);
-        $("#uvIndex").text("UV Index: " + uvIndex);
         
+        // console.log(localStorage.getItem(localStorage.key(0) ));
+        
+
     }
 
 
     function future(){
+        // Date, Icon, Temp, Humidity
 
+        // Date goes here
+        // // weather icon here
+        // $("<div>").text("Temperature: " + temp + " F");
+        // $("<div>").text("Humidity: " + humidity + " %");
+
+        $("#0").append( $("<div>").text("Temp: " + temp + "F") );
+        $("#0").append( $("<div>").text("Humidity: " + humidity + "%") );
 
     }
     
