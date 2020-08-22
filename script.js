71543d8c915aa228db2f14138e71c74987773bef
miadehaan <64979457+miadehaan@users.noreply.github.com>
2020-08-21 23:58:24 -0700

    forecast works (doesn't duplicate) and history buttons now work

--- script.js
+++ script.js
@@ -1,190 +1,185 @@
-$(document).ready(function() {
+// Search parameters
+var city;
+var date;
 
-    // Search parameters
-    var cityInput;
-    var date;
-    var allCities = [];
+// Local Storage
+let history = JSON.parse(window.localStorage.getItem("history")) || [];
 
-    // Weather data
-    var cityName;
-    var country;
-    var temp;
-    var humidity;
-    var windspeed;
-    var uvIndex;
-    var lon;
-    var lat;
-    var currentIcon;
-    var tempForecast;
-    var humidityForecast;
-    var dailyTemp;
-    var dailyHumidity;
+    
+// Weather data
+var cityName;
+var country;
+var temp;
+var humidity;
+var windspeed;
+var uvIndex;
+var currentIcon;
+var lat;
+var lon;
     
 
-    var apiKey = "e68a6c498567148d8870611b117efac1";
-
+$(document).ready(function() {
     // Retrieve the city input by the user
     $(".citySubmit").on("click", function(event){
         event.preventDefault();
 
         // Get the city the user input
-        cityInput = $(".cityInput").val().trim();
-        allCities.push(cityInput); //adds to array
-
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid="+ apiKey + "&units=imperial";
-        // First AJAX call to get lat/lon coordinates based on city input
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function(response) {
-            // console.log(response);
-            
-            localStorage.setItem("city", allCities);
-            cityName = response.name;
-            country = response.sys.country;
-            date = response.dt; //Time of data calculation, unix, UTC
-            // convert dt to actual date:
-
-            lon = response.coord.lon;
-            lat = response.coord.lat;
-
-            
-            // Second/Main AJAX Call to get weather data
-            var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely&appid=e68a6c498567148d8870611b117efac1&units=imperial";
-            //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=daily&appid=e68a6c498567148d8870611b117efac1&units=imperial
-
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            }).then(function(response) {
-                console.log(response);
+        city = $(".cityInput").val().trim();
+        current(city);
+        $(".cityInput").val('');
+    });
     
-                // Get current weather data
-                temp = response.current.temp; 
-                humidity = response.current.humidity;
-                windspeed = response.current.wind_speed;
-                uvIndex = response.current.uvi;
-                currentIcon = response.current.weather[0].icon;
-                
-                // Get data for 5-day forecast
-                for (var i=0; i < 5; i++) {
-                    dailyTemp = response.daily[i].temp.day;
-                    dailyHumidity = response.daily[i].humidity;
+    // Make new button for history
+    for (var i=0; i < history.length; i++) {
+        newButton(history[i]);
+    }
 
-                    // console.log(dailyTemp);
-                    // console.log(dailyHumidity);
 
-                    // Switch statement
-                    switch (i) {
-                        case 0:
-                            $("#0").append( $("<div>").text("Temp: " + dailyTemp + "F") );
-                            $("#0").append( $("<div>").text("Humidity: " + dailyHumidity + "%") );
-                            break;
-
-                        case 1:
-                            $("#1").append( $("<div>").text("Temp: " + dailyTemp + "F") );
-                            $("#1").append( $("<div>").text("Humidity: " + dailyHumidity + "%") );
-                            break;
-
-                        case 2:
-                            $("#2").append( $("<div>").text("Temp: " + dailyTemp + "F") );
-                            $("#2").append( $("<div>").text("Humidity: " + dailyHumidity + "%") );
-                            break;
-
-                        case 3:
-                            $("#3").append( $("<div>").text("Temp: " + dailyTemp + "F") );
-                            $("#3").append( $("<div>").text("Humidity: " + dailyHumidity + "%") );
-                            break;                           
-                            
-                        case 4:
-                            $("#4").append( $("<div>").text("Temp: " + dailyTemp + "F") );
-                            $("#4").append( $("<div>").text("Humidity: " + dailyHumidity + "%") );
-                            break; 
-                    }
-                    
-                    // If id is = to i
-                    // if ( parseInt($(".forecast").attr("id")) === i ) {
-                    //     console.log("Day: " + i);
-                    //     console.log($(this) );
-                    //     // Add data to corresponding day 
-                    //     $("#0").append( $("<div>").text("Temp: " + dailyTemp + "F") );
-                    //     $("#0").append( $("<div>").text("Humidity: " + dailyHumidity + "%") );
-                    // }
-  
-                }
-
-                
-                current();
-                future(); 
-            });
-        }); 
+    // event listener for history buttons
+    $(".historyBtn").on("click", function(event){
+        current( $(this).val() );
     });
 
+});
 
-    function current() {
+function newButton(city) {
+        var historyBtn = $("<button>").addClass("historyBtn");
+        historyBtn.val(city);
+        historyBtn.text(city);
+        $(".historyList").append(historyBtn);
+}
 
-        // Pull this data from localStorage ???
 
+function current(city) {
+    var apiKey = "e68a6c498567148d8870611b117efac1";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid="+ apiKey + "&units=imperial";
+    // First AJAX call to get lat/lon coordinates based on city input
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        // console.log(response);
+
+        // need to set window with a stringify to get back a JSON object
+        while (history.indexOf(city) === -1) {
+            history.push(city);
+            window.localStorage.setItem("history", JSON.stringify(history));
+            newButton(city);
+        }
+
+        cityName = response.name;
+        country = response.sys.country;
+        date = unixTimestamp(response.dt); // convert dt to actual date
+
+        // Get current weather data
+        temp = response.main.temp; 
+        humidity = response.main.humidity;
+        windspeed = response.wind.speed;
+        currentIcon = response.weather[0].icon;
+
+        lon = response.coord.lon;
+        lat = response.coord.lat;
+
+        // Create elements with the weather data
         $("#cityName").text(cityName + ", " + country);
         $(".date").text("(" + date + ")");
         $("#temp").text("Temperature: " + temp + " F");
         $("#humidity").text("Humidity: " + humidity + " %");
         $("#windSpeed").text("Wind Speed: " + windspeed + " mph");
-        $("#uvIndex").text("UV Index: " + uvIndex);
 
+    
         // Add weather Icon
         var iconURL = "http://openweathermap.org/img/wn/" + currentIcon + "@2x.png";
         $(".weatherIcon").attr("src", iconURL);
-
-        
-        
-        // Display the search history and pull info from localStorage
-        var history = JSON.parse(localStorage.getItem("city") ); 
-        // history.concat(localStorage.getItem("city")) ;
-        console.log(history); 
-
-        // for (var i=0; i < history.length; i++) {
-        //     console.log(history[i]);
-        //     var historyBtn = $("<button>").addClass("historyBtn");
-        //     historyBtn.text(history[i]);
-        //     $(".history").append(historyBtn);
-
-        // }
-        // history.attr("border: 1px solid"); // add to css class
-
-        $(history).each( (index, element) => {
-            console.log(index);
-            var historyBtn = $("<button>").addClass("historyBtn");
-            historyBtn.text(element);
-            $(".history").append(historyBtn);
-        });
-
-
-        
     
+        future(city);
+                
+        
+    });
+}
 
-    }
+function unixTimestamp(t) {
+    var date = new Date(t*1000);
+    var months_arr = ['1','2','3','4','5','6','7','8','9','10','11','12'];
+    var month = months_arr[date.getMonth()];
+    var year = date.getFullYear();
+    var day = date.getDate();
+    var dateDisplay = month + '/' + day + '/' + year;
 
+    return dateDisplay;
+}
 
-    function future(){
-        // Date, Icon, Temp, Humidity
+function future(city) {
+    // Date, Icon, Temp, Humidity
+    // Second/Main AJAX Call to get weather data
+    var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely&appid=e68a6c498567148d8870611b117efac1&units=imperial";
+    //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=daily&appid=e68a6c498567148d8870611b117efac1&units=imperial
 
-        // Date goes here
-        // // weather icon here
-        // $("<div>").text("Temperature: " + temp + " F");
-        // $("<div>").text("Humidity: " + humidity + " %");
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        // console.log(response);
 
+        // get UV index
+        uvIndex = response.current.uvi;
+        $("#uvIndex").text("UV Index: ");
+        $(".uvValue").text(uvIndex);
+        
+        if(uvIndex < 3) {
+            $(".uvValue").addClass("uvGreen");
+        } else if(uvIndex >= 3 && uvIndex < 7){
+            $(".uvValue").addClass("uvYellow");
+        } else {
+            $(".uvValue").addClass("uvRed");
+        }
+                
+        $(".forecast").empty();
+        // Get data for 5-day forecast
+        for (var i=0; i < 5; i++) {
+            let day = response.daily[i].dt; // ( date of forecast )
+            let dailyTemp = response.daily[i].temp.day;
+            let dailyHumidity = response.daily[i].humidity;
+            let icon = response.daily[i].weather[0].icon;
+            let iconURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
+            // Switch statement
+            switch (i) {
+                case 0:
+                    $("#0").append( $("<div>").addClass("forecastDate").text(unixTimestamp(day)) );
+                    $("#0").append( $("<img>").attr("src", iconURL) );
+                    $("#0").append( $("<div>").text("Temp: " + dailyTemp + "F") );
+                    $("#0").append( $("<div>").text("Humidity: " + dailyHumidity + "%") );
+                    break;
 
-    }
-    
+                case 1:
+                    $("#1").append( $("<div>").addClass("forecastDate").text(unixTimestamp(day)) );
+                    $("#1").append( $("<img>").attr("src", iconURL) );
+                    $("#1").append( $("<div>").text("Temp: " + dailyTemp + "F") );
+                    $("#1").append( $("<div>").text("Humidity: " + dailyHumidity + "%") );
+                    break;
 
+                case 2:
+                    $("#2").append( $("<div>").addClass("forecastDate").text(unixTimestamp(day)) );
+                    $("#2").append( $("<img>").attr("src", iconURL) );
+                    $("#2").append( $("<div>").text("Temp: " + dailyTemp + "F") );
+                    $("#2").append( $("<div>").text("Humidity: " + dailyHumidity + "%") );
+                    break;
 
-
-
-
-
-
-
-
-});
+                case 3:
+                    $("#3").append( $("<div>").addClass("forecastDate").text(unixTimestamp(day)) );
+                    $("#3").append( $("<img>").attr("src", iconURL) );
+                    $("#3").append( $("<div>").text("Temp: " + dailyTemp + "F") );
+                    $("#3").append( $("<div>").text("Humidity: " + dailyHumidity + "%") );
+                    break;                           
+                    
+                case 4:
+                    $("#4").append( $("<div>").addClass("forecastDate").text(unixTimestamp(day)) );
+                    $("#4").append( $("<img>").attr("src", iconURL) );
+                    $("#4").append( $("<div>").text("Temp: " + dailyTemp + "F") );
+                    $("#4").append( $("<div>").text("Humidity: " + dailyHumidity + "%") );
+                    break; 
+            }
+        }    
+    });
+}
